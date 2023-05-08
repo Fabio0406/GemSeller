3b87e47c4ef28a735f4d5089a389521a95e1651d
fabio <fabioarredondo44@gmail.com>
2023-05-08 00:54:07 -0400

    Gran avance

--- src/controllers/api.CO.js
+++ src/controllers/api.CO.js
@@ -10,9 +10,15 @@ export const Send = async (req,res) =>{
     const fecha = new Date()
     const pro = await pool.query("INSERT INTO carrito(id_cliente,fecha_compra,total) VALUES($1,$2,$3)",[req.user.usuario,fecha.toLocaleDateString('en-US'),req.body.total])
     const idcarr= await pool.query("SELECT id_carrito FROM carrito ORDER BY id_carrito DESC LIMIT 1")
-
     req.body.orderItems.forEach(async element => {
         const Dcarr = await pool.query("INSERT INTO detalle_carrito(id_carrito,id_producto,cantidad,precio) VALUES($1,$2,$3,$4)",[idcarr.rows[0].id_carrito,element.id_producto,element.cantidad,element.precio])
+        const Canti = await pool.query("SELECT * FROM inventario WHERE id_producto = $1"[element.id_producto])
+        const calculo = Canti.rows[0].cantidad_disponible - element.cantidad
+        if(calculo > 0){
+            await pool.query("UPDATE inventario SET cantidad_disponible = $1 WHERE id_inventario = $2",[calculo,Canti.rows[0].id_inventario])
+        }else{
+            res.json({ ok: false, status: 200 , order:false})
+        }
     });
     const Estado = "En Proceso"
     const Env = await pool.query("INSERT INTO envio(id_carrito,direccion,ciudad,estado,pais,codigo_postal,fecha_envio) VALUES($1,$2,$3,$4,$5,$6,$7)",[idcarr.rows[0].id_carrito,req.body.Direccion,req.body.Ciudad,Estado,req.body.Pais,req.body.CodigoPostal,fecha.toLocaleDateString('en-US')])
--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 import pkg from 'pg'
 
 export const pool = new pkg.Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Admin123',
-    database: 'Joyeria',
-    port: '5432'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER ||'postgres',
+    password: process.env.DB_PASSWORD ||'Admin123',
+    database: process.env.DB_DATABASE ||'Joyeria',
+    port: process.env.DB_PORT || '5432'
 })
